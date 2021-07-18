41b8a47a586612d09055c3ea868583af67c3847d
Alex Drover <alexfdrover@gmail.com>
2021-07-18 07:51:13 -0700

    add pagination to Table.js

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Table = (props) => {
   const format = props.format
   const cols = props.columns
   const routes = props.rows
+  const [start, setStart] = useState(0)
+  const PAGE_SIZE = 25
 
   const genKey = route => {
     return `${route.airline}/${route.src}/${route.dest}`
@@ -22,10 +24,22 @@ const Table = (props) => {
   const headerCells = cols.map(col => {
     return <th key={col.name}>{col.name}</th>
   })
+  
+  let message = `Showing ${start} - ${start + PAGE_SIZE} routes of total ${routes.length} routes`
+  const nextPage = () => {
+    if (start === (routes.length - PAGE_SIZE)) return
+    setStart(start + PAGE_SIZE)
+  }
+  
+  const prevPage = () => {
+    if (start === 0) return
+    setStart(start - PAGE_SIZE)
+  }
+
 
   const bodyRows = routes.map(route => {
     return createRow(route)
-  })
+  }).slice(start, start + PAGE_SIZE)
   
   return (
     <div>
@@ -37,6 +51,9 @@ const Table = (props) => {
           {bodyRows}
         </tbody>
       </table>
+      <button onClick={() => prevPage()}>Previous Page</button>
+      {message}
+      <button onClick={() => nextPage()}>Next Page</button>
     </div>
   )
 }
